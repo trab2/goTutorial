205c2cfb65f67f1d657ca902bb3c6d921b1782ba
trab2 <daniel.yazlle@jala-foundation.org>
2022-09-28 16:58:09 -0300

    programa base completado

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	var conferenceName = "Go conference"
 	// para los valores que no van a cambiar se usa const
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets int = 50
 	fmt.Println("welcome to", conferenceName, "booking application")
 	fmt.Println("we have", conferenceTickets, "and remains", remainingTickets)
 	// printf sirve para sring formatting, es necesario agregar \n para el salto de linea
@@ -24,11 +24,20 @@ func main() {
 	var userTickets int
 	// asi se pueden declarar variables (NO CONSTANTES) de esta forma no se puede asignarsele un data type y es lo que interprete GO
 	ejemplo := "sugar"
+	// scan es como input en python, nos pedira que introduzcamos el nombre, el input debe hacerse con un POINTER
+	fmt.Println("ingrese su nombre")
+	fmt.Scan(&userName)
+	// pointer es un tipo de variable especial que nos localiza el lugar de memoria en donde esta almacenada la variable
+	fmt.Println("el pointer de userName es ", &userName)
 
-	userName = "tomacito"
-	userTickets = 2
+	fmt.Println("ingrese cuantos tickets desea")
+	fmt.Scan(&userTickets)
 	// es posible saber los tipos con %T
 	// https://www.geeksforgeeks.org/data-types-in-go/
-	fmt.Printf("user name es %T y user tickets es %T y sugar es %T", userName, userTickets, ejemplo)
+	fmt.Printf("user name es %T y user tickets es %T y sugar es %T\n", userName, userTickets, ejemplo)
+	fmt.Printf("user name es %v y user tickets es %v y sugar es %v\n", userName, userTickets, ejemplo)
+	// es posible hacer esta operacion ya que ambos son int, solo se pueden hacer operaciones en variables iguales
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("quedan %v tickets disponibles", remainingTickets)
 
 }
